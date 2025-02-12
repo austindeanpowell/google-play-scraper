0b4f80c54189a3cdf74d1c081d9546d089fc8036
austindeanpowell <167955520+austindeanpowell@users.noreply.github.com>
2025-02-11 21:48:19 -0800

    Update sortreviews.py
    This file allows you to fetch Google Play App Store Reviews along with: Time, Date, User, Rating, and Sting Comment Formatting it in legible fashion.

--- sortreviews.py
+++ sortreviews.py
@@ -1,13 +1,16 @@
+#JSON formats data in human-readable style to serialize and deserialize, ideal for use in APIs and data storage.
 import json
 from datetime import date, datetime
 from google_play_scraper import Sort, reviews
 
-# Fetch 34 reviews for co_star, only with 5-star ratings
+# Fetch 15 reviews for co_star, only with 5-star ratings
 co_star_reviews, _ = reviews(
+    #Put URL here
     'com.costarastrology',
     lang='en',
     country='us',
     sort=Sort.NEWEST,
+    #Replace Count and filter_Score_with desired outcome
     count=15,
     filter_score_with=3
 )
